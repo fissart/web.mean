e5913c3918af2617091298d203b03c3dd14554b6
fissart <ricardomallqui6@gmail.com>
2022-06-29 17:21:04 -0500

    Initial comment

--- users/src/app/services/themes.service.ts
+++ users/src/app/services/themes.service.ts
@@ -22,155 +22,158 @@ export class ThemesService {
   }
 
   createtheme( unidad: string, curse: string, user:string) {
-    return this.http.post<any>(this.URL, {title:"Título", description:`<h1 class='bg-primary border p-1 mb-1 rounded'>
+    return this.http.post<any>(this.URL, {title:"Título", description:`<div class='h4 border p-1 mb-2'>
 APRENDIZAJE ESPERADO
-</h1>
-<div class='bg-info p-1 mb-2 rounded'>
-<h3 class='bg-light border rounded p-1 mb-1'>
-Capacidades: Embebe objetos
-</h3>
-<h3 class='bg-light border rounded p-1 mb-1'>
-Conocimientos: Conoce embebibimiento
-</h3>
-<h3 class='bg-light border rounded p-1 m-0'>
-Actitudes: Asume responsabilidad
-</h3>
+<ol class='m-0 h6'>
+<li>
+<b>Capacidades:</b> Embebe objetos
+</li>
+<li>
+<b>Conocimientos:</b> Conoce embebibimiento
+</li>
+<li>
+<b>Actitudes:</b> Asume responsabilidad
+Construye el triángulo áureo
+</li>
+</ol>
 </div>
 
 
-<h1 class='bg-primary border p-1 mb-1 rounded'>
-SECUENCIA DIDÁCTICA
-</h2>
-<p class="p-1 m-1 bg-warning">
-Motivación, desarrollo y evaluación permanentes de  actitudes
-</p>
-<div class='bg-info p-1 mb-2 rounded'>
-<h3 class='bg-light border rounded p-1'>
-INICIO (20min) {Despertar el interés - Recuperar saberes previos - Estimular el conflicto cognitivo}. RECURSOS: Diapositiva proyector, pizarra, plumones.
-</h3>
+<div class='h4 border p-1 mb-2'>
+SECUENCIA DIDÁCTICA.
+<i class="p-1  m-0 h6">Motivación, desarrollo y evaluación permanentes de  actitudes.
+</i>
 
-<p class="h6 pb-1">
+<h3 class='border p-1 m-0'>
+<b>INICIO (20min)</b> Despertar el interés - Recuperar saberes previos - Estimular el conflicto cognitivo. <i>RECURSOS: Diapositiva proyector, pizarra, plumones.</i>
+<hr class="my-1">
+<p class="h6 pb-1 h5 m-0">
 Estructuras planas y tridimensionales tipos
 </p>
-
-<h3 class='bg-light border rounded  p-1'>
-DESARROLLO (20min) {Adquirir información - Aplicar - Transferir lo aprendido}. RECURSOS: Diapositiva proyector, pizarra, plumones.
 </h3>
 
-<p class="h6 pb-1">
+<h3 class='border p-1 my-1'>
+<b>DESARROLLO (20min)</b> Adquirir información - Aplicar - Transferir lo aprendido. <i>RECURSOS: Diapositiva proyector, pizarra, plumones.</i>
+<hr class="my-1">
+<p class="h6">
 Desarrollo de la sesión construcción del triángulo áureo y uso de geogebra
 </p>
-
-<h3 class='bg-light border rounded  p-1'>
-CIERRE (20min)  {Reflexionar sobre el proceso de aprendizaje}. RECURSOS: Diapositiva proyector, pizarra, plumones.
 </h3>
 
-<p class="h6 pb-1">
+<h3 class='border p-1 m-0'>
+<b>CIERRE (20min)</b>  Reflexionar sobre el proceso de aprendizaje. <i>RECURSOS: Diapositiva proyector, pizarra, plumones.</i>
+<hr class="my-1">
+<p class="h6">
 Se realiza preguntas retroalimentativas ¿Qué otros estructuras bidimensionales y tridimensionales podrían utilizar?
 ¿Compondría estructuras más complejas en bases a esta estructura?
 </p>
+</h3>
 
 </div>
 
-<h1 class='bg-primary border p-1 mb-1 rounded'>
+<div class='h4 border p-1 mb-2'>
 EVALUACIÓN
-</h1>
+<h3 class='bg-light p-1 mb-1'>
+<b>CRITERIOS DE EVALUACIÓN DE LOS APRENDIZAJES:</b>
+Formula y construye una estructura según pautas establecidas
+<hr class="my-0">
+<b>Indicadores de evaluación</b>
+<ol class='m-0 h6'>
+<li>Construye el triángulo áureo</li>
+<li>Compone en base al estructura del triángulo áureo
+</li>
+</ol>
 
-<div class='bg-success text-light p-1 mb-2 rounded'>
-<h3 class='bg-info border rounded p-1 mb-1'>
-CRITERIOS DE EVALUACIÓN DE LOS APRENDIZAJES
+<b>Instrumentos</b>
+<ol class='m-0 h6'>
+<li>Ficha de observación
+</li>
+<li>Lista de cotejo
+</li>
+</ol>
 </h3>
-<p>Formula y construye una estructura según pautas establecidas</p>
-<p>INDICADORES DE EVALUACIÓN</p>
-<ol>
-<li>
-Construye el triángulo áureo
+
+<h3 class='bg-light p-1 mb-1'>
+<b>ACTITUD ANTE EL ÁREA:</b> Presta atención y participa en las actividades
+<hr class="my-0">
+<b>Indicadores de evaluación</b>
+<ol class='m-0 h6'>
+<li>Presta atención
 </li>
-<li>
-Compone en base al estructura del triángulo áureo
+<li>Participa activamente
+</li>
+<li>Colabora con sus compañeros
 </li>
 </ol>
-<h3 class='bg-info border rounded p-1 mb-1'>
-ACTITUD ANTE EL ÁREA
+
+<b>Instrumentos</b>
+<ol class='m-0 h6'>
+<li>Prueba  escrita
+</li>
+<li>Lista de cotejo
+</li>
+</ol>
 </h3>
-<p>Presta atención y participa en las actividades</p>
-<p>INDICADORES DE EVALUACIÓN</p>
-<ol>
-<li>
-Presta atención
-</li>
-<li>
-Participa activamente
-</li>
-<li>
-Colabora con sus compañeros
-</li>
-</ol>
-<p>INSTRUMENTOS DE EVALUACIÓN</p>
-<ol>
-<li>
-Lista de cotejo
-</li>
-<li>
-Ficha de obervación
-</li>
-</ol>
-<p>INSTRUMENTOS DE EVALUACIÓN</p>
-<ol>
-<li>
-Lista de cotejo
-</li>
-<li>
-Ficha de obervación
-</li>
-</ol>
+</h3>
+
 </div>
 
-<h1 class='bg-primary border p-1 mb-1 rounded'>
-REFERENCIAS
-</h1>
+<div class='h4 border p-1 mb-2'>
+BIBLIOGRAFÍA
 
-<div class='bg-light p-1 mb-2 rounded'>
 
-<ol class='bg-light m-0'>
+
+
+<ol class='m-0 h6'>
 <li>
-<a class="btn btn-outline-warning" href="https://drive.google.com/file/u/0/d/0B1Ga40SmVyy-TTRWcXNkRGYxY00/view?resourcekey=0-tXQ867e7G7OY2bMQzshTKQ" target="_blanck">Libro</a>
+<a class="btn btn-info mb-1" href="https://drive.google.com/file/u/0/d/0B1Ga40SmVyy-TTRWcXNkRGYxY00/view?resourcekey=0-tXQ867e7G7OY2bMQzshTKQ" target="_blanck">Libro de matemáticas aplicadas</a>
 </li>
-<br>
 <li>
-<a class="btn btn-outline-warning"
+<a class="btn btn-info mb-1"
  href="https://marcianosmx.com/10-fractales-alucinantes-puedes-encontrar-la-naturaleza/" target="_blanck">Sucesiones en la naturaleza</a>
 </li>
-<br>
+
 <li>
-<a class="btn btn-outline-warning"
+<a class="btn btn-info mb-0"
  href="https://marcianosmx.com/10-fractales-alucinantes-puedes-encontrar-la-naturaleza/" target="_blanck">Sucesiones</a>
 </li>
 </ol>
 
 </div>
 
-<h1 class='bg-primary border p-1 mb-1 rounded'>
+<div class='h4 border p-1 mb-2'>
 ANEXOS
-</h1>
-<div class='bg-info p-1 mb-2 rounded'>
-<h3 class='bg-light border rounded p-1 mb-1'>
-INSTRUMENTOS
-</h3>
-<iframe width="100%" height="700" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=663B3002C660A66%213145&authkey=%21ACU-c7Jq42zc-jE&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"></iframe>
-<hr>
-<iframe width="100%" height="700" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=663B3002C660A66%213143&authkey=%21ANASoDTDFp4_JAg&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=false&ejss=false"></iframe>
-<hr>
-<iframe src="https://onedrive.live.com/embed?cid=0663B3002C660A66&resid=663B3002C660A66%213147&authkey=ALNWzY_Vt1xzS9c&em=2" width="100%" height="500" frameborder="0" scrolling="no"></iframe>
-<h3 class='bg-light border rounded p-1 mt-2 mb-0'>
-MATERIALES
-</h3>
-<img class='bg-light p-1 m-auto' src="https://thumbs.dreamstime.com/b/elegant-blue-background-swirls-space-your-text-elegant-blue-background-swirls-space-your-text-159291073.jpg" width="100%"
-/>
-<h3 class='bg-light border rounded p-1 mt-2 mb-0'>
-VIDEOS
-</h3>
-<iframe width="100%" height="315" src="https://www.youtube.com/embed/ZnuwB35GYMY?start=16" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<div class='h5 border p-1 mb-2'>
+Materiales
+<ol class="mb-0">
+<li><a  class="btn btn-info mb-1" target="_blank" href="https://www.geogebra.org/classic">
+Geogebra Web
+</a></li>
+<li><a  class="btn btn-info mb-1" target="_blank" href="http://asymptote.ualberta.ca/">
+Asymptote Web
+</a></li>
+<li><a  class="btn btn-info mb-1" target="_blank" href="https://sketchfab.com/">
+Sketchfab plataforma 3d
+</a></li>
+<li><a  class="btn btn-info mb-1" target="_blank" href="https://app.sketchup.com/app?hl=en">
+Sketchup Web
+</a></li>
+<li><a  class="btn btn-info mb-1" target="_blank" href="https://blender.org">
+Blender
+</a></li>
+<li><a  class="btn btn-info mb-1" target="_blank" href="http://fractales.cf">
+fractales.cf
+</a></li>
+</ol>
+</div>
+
+<div class='h5 border p-1 mb-2'>
+Videos
+<hr class="my-1">
+<a class="btn btn-info mb-1" href="https://studio.youtube.com/channel/UCL5FCvd7ZTtecFKf8nMyn8Q/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D" target="_blanck">Canal youtube</a>
+
+</div>
 </div>`, task:"Tarea"  , time:"2022-12-12", unidad, curse, user });
   }
   updateTheme(id: string, title: string, description: string, task: string, time: string, filew:File) {
